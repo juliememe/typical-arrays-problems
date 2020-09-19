06346c273bec9c65651ca77ce21641771f5ece45
julie_meme <julie_meme@mail.ru>
2020-09-19 19:01:40 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (!(Array.isArray(array) && array.length > 1)) {
+      return 0;
+    } else {
+      return Math.min.apply(null, array);
+    }
+  }
+  
+  
+  
+  
+  exports.max = function max(array) {
+    if (!(Array.isArray(array) && array.length > 1)) {
+      return 0;
+    } else {
+      return Math.max.apply(null, array);
+    }
+  }
+  
+  
+  
+  exports.avg = function avg(array) {
+    let sum = 0;
+    if (!(Array.isArray(array) && array.length > 1)) {
+      return 0;
+    } else {
+      for (let i = 0; i <= array.length - 1; i++) {
+        sum += array[i];
+      }
+    }
+    return (Math.ceil((sum / array.length) * 100) / 100);
+  }
+  
